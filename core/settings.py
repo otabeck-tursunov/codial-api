4151219759b909ecffbb60a3cb07e01e5774520b
unknown <otabecktursunov@gmail.com>
2026-01-29 13:01:43 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#@(u)z^qd2e6(773f_+121y$4tjeq-8qjvukmmvl_!o1234-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
